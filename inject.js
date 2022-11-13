38ed65a71e64e290d93c921a9cf24a9991fb854e
Griffin Hurt <griffinhurt18@outlook.com>
2022-11-13 18:28:00 -0500

    Added SB schedules and favorites to interval

--- inject.js
+++ inject.js
@@ -2,26 +2,32 @@ window.setInterval(() => {
     try {
         const ifr = document.getElementById("main_iframe")
         const d = ifr.contentWindow.document;
+
+        // Get the page name
+        const pn = getPageName(d)
+
         // Checking if on the schedule page
-        if (getPageName(d) === "Schedule") {
+        if (pn === "Schedule") {
             updateScheduleProfessors(d)
         // Checking if in the course catalogue
-        } else if (getPageName(d).match(/([A-Z]+) (\d{4}) - (.+)/g)) {
+        } else if (pn.match(/([A-Z]+) (\d{4}) - (.+)/g)) {
             // Get the name of the subject
-            const mat = getPageName(d).match(/([A-Z]+) (\d{4}) - (.+)/)
+            const mat = pn.match(/([A-Z]+) (\d{4}) - (.+)/)
             const subjectName = mat[1].toUpperCase()
             const subjectNumber = parseInt(mat[2])
             updateCourseCatalogProfessors(d, subjectName, subjectNumber)
-        } else if (getPageName(d) === "Class Search") {
+        } else if (pn === "Class Search") {
             updateCourseSearchProfessors(d)
-        } else if (getPageName(d) === "Shopping Cart") {
+        } else if (pn === "Shopping Cart") {
             updateShoppingCart(d)
-        } else if (getPageName(d) === "Edit Enrollment") {
+        } else if (pn === "Edit Enrollment") {
             updateEditEnrollment(d)
-        } else if (getPageName(d) === "Drop Classes") {
+        } else if (pn === "Drop Classes") {
             updateDropClasses(d)
-        } else if (getPageName(d) === "Select Sections") {
+        } else if (pn === "Select Sections") {
             updateSBSelectSections(d)
+        } else if (pn === "Schedules" || pn === "Favorites") {
+            updateSBSchedules(d)
         }
     } catch (e) {
         ;
