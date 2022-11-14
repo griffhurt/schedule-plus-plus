fdbc864330b66eb367a18511c72fe79ad4fd6bf8
Griffin Hurt <griffinhurt18@outlook.com>
2022-11-14 09:02:03 -0500

    Added a configuration file

---
+++ data/config.js
@@ -0,0 +1,10 @@
+// Configuration File
+
+// The link to the CORS proxy being used
+const SPP_CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
+// The link to RMP's GraphQL API
+const SPP_GRAPHQL_LINK = "https://www.ratemyprofessors.com/graphql"
+
+// Authentication for the GraphQL API
+const SPP_USERNAME = "test"
+const SPP_PASSWORD = "test"
