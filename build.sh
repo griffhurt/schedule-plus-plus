bd042defb76bd6eb44c4e0e6d689720e168756d0
Griffin Hurt <griffinhurt18@outlook.com>
2022-11-16 15:55:31 -0500

    Changed build with new data directory

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ cp manifest.v3.json build/chrome/manifest.json
 
 # Copy the files to firefox
 cp inject.js build/firefox/inject.js
-cp -R data/ build/firefox/data
+cp -R spp_data/ build/firefox/spp_data
 cp -R icon/ build/firefox/icon
 cp -R scripts/ build/firefox/scripts
 
 # Copy the files to chrome
 cp inject.js build/chrome/inject.js
-cp -R data/ build/chrome/data
+cp -R spp_data/ build/chrome/spp_data
 cp -R icon/ build/chrome/icon
 cp -R scripts/ build/chrome/scripts
